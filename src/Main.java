27daa2315a814395d10777890eba25af0920bac0
Raouf <raoufmotezarre26@gmail.com>
2025-06-04 11:57:57 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        String name = "Raouf";
+        int age = 47;
+        float temprature = 21.5f;
+        double temprature2 = 22.7;
+        boolean isRaining = false;
     }
 
 }
